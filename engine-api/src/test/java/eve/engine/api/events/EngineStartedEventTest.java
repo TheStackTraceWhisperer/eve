3c18c9ff5a0583942a3ca7eaeb715b628abcee4f
Samuel Hyman <Samuel.L.Hyman@gmail.com>
2025-09-26 16:45:47 -0500

    tests + jacoco v1

---
+++ engine-api/src/test/java/eve/engine/api/events/EngineStartedEventTest.java
@@ -0,0 +1,10 @@
+package eve.engine.api.events;
+
+import org.junit.jupiter.api.Test;
+
+public class EngineStartedEventTest {
+  @Test
+  void test() {
+    new EngineStartedEvent(0);
+  }
+}
---
+++ example-plugin/src/test/java/eve/plugin/ExamplePluginTest.java
@@ -0,0 +1,12 @@
+package eve.plugin;
+
+import org.junit.jupiter.api.Test;
+
+public class ExamplePluginTest {
+
+  @Test
+  void test() {
+
+  }
+
+}

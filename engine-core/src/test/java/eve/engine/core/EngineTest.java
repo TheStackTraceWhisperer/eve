129a9aad1fbc2b4cf4131149378ff06cc2c76fea
Samuel Hyman <Samuel.L.Hyman@gmail.com>
2025-09-26 17:41:45 -0500

    test v4

--- engine-core/src/test/java/eve/engine/core/EngineTest.java
+++ engine-core/src/test/java/eve/engine/core/EngineTest.java
@@ -2,11 +2,19 @@ package eve.engine.core;
 
 import eve.engine.api.Plugin;
 import io.avaje.inject.test.TestBeanScope;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import static org.mockito.Mockito.verify;
 
 class EngineTest {
+
+  @BeforeAll
+  static void beforeAll() {
+    SLF4JBridgeHandler.removeHandlersForRootLogger();
+    SLF4JBridgeHandler.install();
+  }
 
   public static class TestPlugin implements Plugin {
 
